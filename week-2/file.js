7d969371efec91dbd9fc2818683bea1a6db6bff6
Tridib11 <tridibghosh12345@gmail.com>
2024-04-12 02:30:55 +0530

    File added

---
+++ week-2/file.js
@@ -0,0 +1,15 @@
+const fs =require('fs')
+
+function callback(err,data){
+  if(err){
+    console.log(err)
+  }
+  else{
+    console.log(data)
+  }
+}
+
+fs.readFile("a.txt","utf-8",callback)
+console.log("Hello world 1")
+// const data=fs.readFileSync("a.txt","utf-8")
+// console.log(data)
