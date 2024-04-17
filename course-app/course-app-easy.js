1d20558ae3f36e6274b007dc894da7371704b044
Tridib11 <tridibghosh12345@gmail.com>
2024-04-17 08:43:11 +0530

    Refactor course-app/course-app-easy.js to include POST route for admin signup

--- course-app/course-app-easy.js
+++ course-app/course-app-easy.js
@@ -1,9 +1,21 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(bod);
+let ADMINS = [];
+let USERS = [];
+let COURSES = [];
 
-let ADMINS=[]
-let USERS=[]
-let COURSES=[]
+//POST Routes
 
+app.post("/admin/Signup", (req, res) => {
+  const admin = req.body;
+  const existingAdmin = ADMINS.find((a) => a.username === admin.username);
+  if (existingAdmin) {
+    res.status(403).json({ message: "Admin already exists" });
+  } else {
+    ADMINS.push(admin);
+    res.status(203).json({ message: "Admin created successfully" });
+  }
+});
