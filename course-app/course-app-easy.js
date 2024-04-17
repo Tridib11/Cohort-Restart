39c7ff75e0627d1b91669b5ba76237d918ff1bc2
Tridib11 <tridibghosh12345@gmail.com>
2024-04-17 08:54:48 +0530

    Add admin authentication middleware and POST route for adding courses in course-app/course-app-easy.js

--- course-app/course-app-easy.js
+++ course-app/course-app-easy.js
@@ -7,6 +7,20 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+
+//Authentication middlewares
+
+const adminAuthentication=(req,res,next)=>{
+  const {username,password}=req.headers;
+  const admin=ADMINS.find(a => a.username===username && a.password===password)
+  if(admin){
+    next();
+  }else{
+    res.status(403).json({message:'Admin authentication failed'})
+  }
+}
+
+
 //POST Routes
 
 app.post("/admin/Signup", (req, res) => {
@@ -19,3 +33,14 @@ app.post("/admin/Signup", (req, res) => {
     res.status(203).json({ message: "Admin created successfully" });
   }
 });
+
+app.post('/admin/login',adminAuthentication,(req,res)=>{
+  res.status(203).json({message:'Login Successfull'})
+})
+
+app.post('/admin/courses',adminAuthentication,(req,res)=>{
+  const course=req.body
+  course.id=Date.now()
+  COURSES.push(course)
+  res.json({message:`Course added Successfully ID: ${course.id}`})
+})
