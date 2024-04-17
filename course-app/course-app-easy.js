7496073b0aef4dca56723339e6b98c42034f27f0
Tridib11 <tridibghosh12345@gmail.com>
2024-04-17 09:16:21 +0530

    Refactor adminAuthentication middleware and add body-parser dependency in course-app/course-app-easy.js

--- course-app/course-app-easy.js
+++ course-app/course-app-easy.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.json());
-app.use(bod);
+app.use(bodyParser.json());
+
+
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
