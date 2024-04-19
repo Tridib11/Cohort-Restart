7478305ecc2fbe38041e683704a1546813ac3d06
Tridib11 <tridibghosh12345@gmail.com>
2024-04-19 12:56:25 +0530

    Refactor admin authentication routes in course-app/course-app-jwt.js

--- course-app/course-app-jwt.js
+++ course-app/course-app-jwt.js
@@ -9,26 +9,52 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
-const userSecret="userSUP3RS3CR3T"
-const adminSecret="adminSUP3RS3CR3T"
+const userSecret = "userSUP3RS3CR3T";
+const adminSecret = "adminSUP3RS3CR3T";
 
-const generateAdminJwt=(admin)=>{
-  const payload={username:admin.username}
-  return jwt.sign(payload,adminSecret,{expiresIn:'1h'})
-}
+const generateAdminJwt = (admin) => {
+  const payload = { username: admin.username };
+  return jwt.sign(payload, adminSecret, { expiresIn: "1h" });
+};
 
-app.post('/admin/signup',(req,res)=>{
-  const admin=req.body
-  const existingAdmin=ADMINS.find(a=>a.username === admin.username)
-  if(existingAdmin){
-    res.status(403).json({message:"Admin already exists"})
-  }else{
-    ADMINS.push(admin)
-    const token=generateAdminJwt(admin)
-    res.status(203).json({message:"Admin created successfully",token})
+const authnticateAdminJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, adminSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      next();
+    });
+  } else {
+    res.sendStatus(401);
   }
-})
+};
 
-app.post('/admin/signin',(req,res)=>{
-  const admin=
-})
+app.post("/admin/signup", (req, res) => {
+  const admin = req.body;
+  const existingAdmin = ADMINS.find((a) => a.username === admin.username);
+  if (existingAdmin) {
+    res.status(403).json({ message: "Admin already exists" });
+  } else {
+    ADMINS.push(admin);
+    const token = generateAdminJwt(admin);
+    res.status(203).json({ message: "Admin created successfully", token });
+  }
+});
+
+app.post("/admin/signin", (req, res) => {
+  const { username, password } = req.headers;
+  const admin = ADMINS.find(
+    (a) => a.username === username && a.password === password
+  );
+  if (admin) {
+    const token = generateAdminJwt(admin);
+    res.status(203).json({ message: "Admin Loggedin successfully", token });
+  } else {
+    res.status(404).json({ message: "Admin authentication failed" });
+  }
+});
+
+app.post("/admin/courses", authnticateAdminJwt, (req, res) => {});
