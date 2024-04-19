ee7e1b0b9c0b488f4ea0554258975ed8a4e7eb04
Tridib11 <tridibghosh12345@gmail.com>
2024-04-19 12:46:42 +0530

    Add admin signin route in course-app/course-app-jwt.js

--- course-app/course-app-jwt.js
+++ course-app/course-app-jwt.js
@@ -12,8 +12,23 @@ let COURSES = [];
 const userSecret="userSUP3RS3CR3T"
 const adminSecret="adminSUP3RS3CR3T"
 
+const generateAdminJwt=(admin)=>{
+  const payload={username:admin.username}
+  return jwt.sign(payload,adminSecret,{expiresIn:'1h'})
+}
 
 app.post('/admin/signup',(req,res)=>{
   const admin=req.body
-  
+  const existingAdmin=ADMINS.find(a=>a.username === admin.username)
+  if(existingAdmin){
+    res.status(403).json({message:"Admin already exists"})
+  }else{
+    ADMINS.push(admin)
+    const token=generateAdminJwt(admin)
+    res.status(203).json({message:"Admin created successfully",token})
+  }
+})
+
+app.post('/admin/signin',(req,res)=>{
+  const admin=
 })
