6d5dd8621525a4858640948752d910225938eb42
Tridib11 <tridibghosh12345@gmail.com>
2024-04-19 12:40:47 +0530

    Add admin signup route in course-app/course-app-jwt.js

--- course-app/course-app-jwt.js
+++ course-app/course-app-jwt.js
@@ -8,3 +8,12 @@ app.use(bodyParser.json());
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
+
+const userSecret="userSUP3RS3CR3T"
+const adminSecret="adminSUP3RS3CR3T"
+
+
+app.post('/admin/signup',(req,res)=>{
+  const admin=req.body
+  
+})
