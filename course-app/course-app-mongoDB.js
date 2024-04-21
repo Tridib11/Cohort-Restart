dffdfc4c93a91678c3003e29ca9c88f702507cf5
Tridib11 <tridibghosh12345@gmail.com>
2024-04-21 09:05:50 +0530

    Refactor code formatting and add missing semicolons in course-app/course-app-mongoDB.js

--- course-app/course-app-mongoDB.js
+++ course-app/course-app-mongoDB.js
@@ -1,47 +1,54 @@
-const express=require('express')
-const jwt=require('jsonwebtoken')
-const mongoose=require('mongoose')
-const app=express()
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const app = express();
 
-aap.use(express.json())
+aap.use(express.json());
 
 //Defining mongoose Schema
 
-const userSchema=new mongoose.Schema({
-    username:String,
-    password:String,
-    purchasedCourses:[{type: mongoose.Schema.Types.ObjectId,ref:'Course'}]
-})
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+});
 
-const adminSchema=new mongoose.Schema({
-  username:String,
-  password:String
-})
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
 
-const courseSchema=new mongoose.Schema({
-  title:String,
-  description:String,
-  price:Number,
-  imageLink:String,
-  published:Boolean
-})
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageLink: String,
+  published: Boolean,
+});
 
 //Defining mongoose models
 
-const User=mongoose.model('User',userSchema)
-const Admin=mongoose.model('Admin',adminSchema)
-const Course=mongoose.model('Course',courseSchema)
-
+const User = mongoose.model("User", userSchema);
+const Admin = mongoose.model("Admin", adminSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 //Secrets
 
-const userSecret="userSUP3RS3CR3T"
-const adminSecret="adminSUP3RS3CR3T"
+const userSecret = "userSUP3RS3CR3T";
+const adminSecret = "adminSUP3RS3CR3T";
 
-const authenticateAdminJwt=(req,res,next)=>{
-  const authHeader=req.headers.authorization
-  if(authHeader){
-    const token=authHeader.split(' ')[1]
-    
+const authenticateAdminJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, adminSecret, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
   }
-}
+};
