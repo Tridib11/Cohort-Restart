6a588cc8786ddef3718318737aab264f15190982
Tridib11 <tridibghosh12345@gmail.com>
2024-04-27 22:26:02 +0530

    Refactor App component to use todo state and add Lol component

--- frontend-todo-app/src/App.jsx
+++ frontend-todo-app/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState({
+    title: "Gym",
+    description: "GO out to the gym",
+    id: 1,
+  });
 
+  setInterval(() => {
+    setTodo({
+      title: "Maan",
+      description: "run",
+      id: 3,
+    });
+  }, 2000);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {todo.title} <br />
+      {todo.description} <br />
+      {todo.id} <br />
+      <Lol name={"Tridib"} desc={"desc"} />
     </>
-  )
+  );
 }
 
-export default App
+function Lol({ name, desc }) {
+  return (
+    <div>
+      {name}
+      <br />
+      {desc}
+    </div>
+  );
+}
+
+export default App;
