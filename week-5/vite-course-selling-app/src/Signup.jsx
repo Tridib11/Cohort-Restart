7f2967eacaeb12e5b369f1dc24f34dcd8c90d8f5
Tridib11 <tridibghosh12345@gmail.com>
2024-05-01 02:48:05 +0530

    Refactor Signup component to center and style in Signup.jsx

--- week-5/vite-course-selling-app/src/Signup.jsx
+++ week-5/vite-course-selling-app/src/Signup.jsx
@@ -1,38 +1,46 @@
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import {Card, Typography} from "@mui/material";
+import { Card, Typography } from "@mui/material";
+
 function Signup() {
   return (
-    <div>
-      <center>
-        <div style={{ marginTop: "150px", marginBottom: "10px" }}>
-            <Typography variant={"h6"}>
-                Welcome to Coursera Signup above
-            </Typography>
-
-        </div>
-      </center>
-      <center>
-          <Card variant={"outlined"} style={{width:"400px", padding:"20px"}} >
-              <TextField
-                  fullWidth
-                  id="outlined-basic"
-                  label="Email"
-                  variant="outlined"
-              />
-              <br /> <br />
-              <TextField
-                  fullWidth
-                  id="outlined-basic"
-                  label="Password"
-                  variant="outlined"
-                  type={"password"}
-              />
-              <br />
-              <br />
-              <Button size={"large"} variant="contained">Sign Up</Button>
-          </Card>
-      </center>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div>
+        <Typography
+          variant={"h6"}
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          Welcome to Coursera Signup above
+        </Typography>
+        <Card variant={"outlined"} style={{ width: "400px", padding: "20px" }}>
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+          />
+          <br /> <br />
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            type={"password"}
+          />
+          <br />
+          <br />
+          <Button size={"large"} variant="contained">
+            Sign Up
+          </Button>
+        </Card>
+      </div>
     </div>
   );
 }
