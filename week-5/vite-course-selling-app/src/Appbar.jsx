d7819bd559503a753a8c1b274d9f32262078b2bd
Tridib11 <tridibghosh12345@gmail.com>
2024-05-01 02:59:53 +0530

    Update Appbar component to include Coursera title and Sign up and Sign in buttons

--- week-5/vite-course-selling-app/src/Appbar.jsx
+++ week-5/vite-course-selling-app/src/Appbar.jsx
@@ -1,5 +1,12 @@
+import {Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+
 function Appbar() {
-  return <div></div>;
+  return <div>
+    <Typography>Coursera</Typography>
+    <Button>Sign up</Button>
+    <Button>Sign in</Button>
+  </div>;
 }
 
 export default Appbar;
