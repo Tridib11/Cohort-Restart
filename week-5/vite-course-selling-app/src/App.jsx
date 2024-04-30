36bf67ea66fee4d4e51e998caf2264875c34dd91
Tridib11 <tridibghosh12345@gmail.com>
2024-05-01 02:55:14 +0530

    Refactor App component to add Appbar and center and style Signup component

--- week-5/vite-course-selling-app/src/App.jsx
+++ week-5/vite-course-selling-app/src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Signup from "./Signup";
+import Appbar from "./Appbar.jsx";
 function App() {
   return (
-    <div style={{width:"100vw", height:"100vh", backgroundColor:"#eeeeee"}}>
+    <div
+      style={{ width: "100vw", height: "100vh", backgroundColor: "#eeeeee" }}
+    >
+      <Appbar></Appbar>
       <Signup></Signup>
     </div>
   );
