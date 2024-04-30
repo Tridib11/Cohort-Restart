da209884212d169da815e04dd6d348b869564a43
Tridib11 <tridibghosh12345@gmail.com>
2024-05-01 02:48:02 +0530

    Refactor App component to wrap Signup component in a div with background color in App.jsx

--- week-5/vite-course-selling-app/src/App.jsx
+++ week-5/vite-course-selling-app/src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Signup from "./Signup";
 function App() {
   return (
-    <>
+    <div style={{width:"100vw", height:"100vh", backgroundColor:"#eeeeee"}}>
       <Signup></Signup>
-    </>
+    </div>
   );
 }
 
