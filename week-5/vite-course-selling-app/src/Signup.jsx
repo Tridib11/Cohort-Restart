7300179a0c3ed55c6033537725b1a7f0ec755152
Tridib11 <tridibghosh12345@gmail.com>
2024-05-01 00:29:00 +0530

    Refactor Signup component in Signup.jsx

--- week-5/vite-course-selling-app/src/Signup.jsx
+++ week-5/vite-course-selling-app/src/Signup.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Signup() {
   return (
-    <div>
-      
+    <div style={{
+      border:"2px solid black"
+    }}>
+      Username - <input type={"text"} />
+      <br />
+      Password - <input type={"password"} />
+      <br />
+      <button>Signup</button>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
