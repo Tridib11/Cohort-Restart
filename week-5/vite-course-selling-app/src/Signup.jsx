913aff074bd018645661f7694bf060b3fea2b979
Tridib11 <tridibghosh12345@gmail.com>
2024-05-01 00:30:20 +0530

    Refactor Signup component to center and style in Signup.jsx

--- week-5/vite-course-selling-app/src/Signup.jsx
+++ week-5/vite-course-selling-app/src/Signup.jsx
@@ -2,14 +2,21 @@ import React from "react";
 
 function Signup() {
   return (
-    <div style={{
-      border:"2px solid black"
-    }}>
-      Username - <input type={"text"} />
-      <br />
-      Password - <input type={"password"} />
-      <br />
-      <button>Signup</button>
+    <div>
+      <center>
+        <div
+          style={{
+            border: "2px solid black",
+            width: "400px",
+          }}
+        >
+          Username - <input type={"text"} />
+          <br />
+          Password - <input type={"password"} />
+          <br />
+          <button>Signup</button>
+        </div>
+      </center>
     </div>
   );
 }
