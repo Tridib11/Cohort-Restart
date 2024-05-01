a62482f74bb8839896040aef50cb3d9e52c4b6d1
Tridib11 <tridibghosh12345@gmail.com>
2024-05-02 00:59:30 +0530

    File added

--- week-5/vite-course-selling-app/src/App.jsx
+++ week-5/vite-course-selling-app/src/App.jsx
@@ -1,14 +1,15 @@
 import Signup from "./Signup";
 import Appbar from "./Appbar.jsx";
+
 function App() {
-  return (
-    <div
-      style={{ width: "100vw", height: "100vh", backgroundColor: "#eeeeee" }}
-    >
-      <Appbar></Appbar>
-      <Signup></Signup>
-    </div>
-  );
+    return (
+        <div
+            style={{width: "100vw", height: "100vh", backgroundColor: "#eeeeee"}}
+        >
+            <Appbar></Appbar>
+            <Signup></Signup>
+        </div>
+    );
 }
 
 export default App;
