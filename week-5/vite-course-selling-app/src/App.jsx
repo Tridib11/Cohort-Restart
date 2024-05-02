0d40ce3d205c56752a2db2b6555be784b689bf54
Tridib11 <tridibghosh12345@gmail.com>
2024-05-02 16:23:09 +0530

    File added

--- week-5/vite-course-selling-app/src/App.jsx
+++ week-5/vite-course-selling-app/src/App.jsx
@@ -1,12 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from "./Signin.jsx";
 import Signup from "./Signup.jsx";
-
-
-// This file shows how you can do routing in React.
-// Try going to /login, /register, /about, /courses on the website and see how the html changes
-// based on the route.
-// You can also try going to /random and see what happens (a route that doesnt exist)
 function App() {
     return (
         <Router>
