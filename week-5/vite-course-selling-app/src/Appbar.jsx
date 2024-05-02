7fcfbeeb788013347d346fcd70bc3ff46ee9412d
Tridib11 <tridibghosh12345@gmail.com>
2024-05-02 16:30:27 +0530

    File added

--- week-5/vite-course-selling-app/src/Appbar.jsx
+++ week-5/vite-course-selling-app/src/Appbar.jsx
@@ -1,25 +1,37 @@
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 
 function Appbar() {
-    return <div style={{
-        display: "flex", justifyContent: "space-between", padding: "4px"
-    }}>
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "4px",
+      }}
+    >
+      <div>
+        <Typography variant={"h5"}>Coursera</Typography>
+      </div>
+      <div style={{ display: "flex" }}>
+        <div style={{ marginRight: "10px" }}>
+          <Button
+            variant={"contained"}
+            onClick={() => {
+              window.location = "/signup";
+            }}
+          >
+            Sign up
+          </Button>
+        </div>
         <div>
-            <Typography variant={"h5"}>Coursera</Typography>
+          <Button variant={"contained"} onClick={()=>{
+              window.location="/signin"
+          }}>Sign in</Button>
         </div>
-        <div style={{display: "flex"}}>
-            <div style={{marginRight: "10px"}}>
-                <Button variant={"contained"}>Sign up</Button>
-            </div>
-            <div>
-                <Button variant={"contained"}>Sign in</Button>
-            </div>
-
-        </div>
-
-
-    </div>;
+      </div>
+    </div>
+  );
 }
 
 export default Appbar;
