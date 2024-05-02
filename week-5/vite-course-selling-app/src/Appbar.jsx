850baac016e4fa3257dd1cd50664a5660367bbb2
Tridib11 <tridibghosh12345@gmail.com>
2024-05-02 15:56:05 +0530

    File added

--- week-5/vite-course-selling-app/src/Appbar.jsx
+++ week-5/vite-course-selling-app/src/Appbar.jsx
@@ -2,21 +2,20 @@ import {Typography} from "@mui/material";
 import Button from "@mui/material/Button";
 
 function Appbar() {
-    return (
-        <>
-            <div>
-                <Typography>Coursera</Typography>
-            </div>
-            <div style={{
-                display: "flex",
-                justifyContent: "flex-end",
-                marginRight:"100px"
-            }}>
-                <Button>Sign up</Button>
-                <Button>Sign in</Button>
-            </div>
-        </>)
+    return <div style={{
+        display: "flex",
+        justifyContent: "space-between"
+    }}>
+        <div>
+            <Typography>Coursera</Typography>
+        </div>
+        <div>
+            <Button>Sign up</Button>
+            <Button>Sign in</Button>
+        </div>
 
+
+    </div>;
 }
 
 export default Appbar;
