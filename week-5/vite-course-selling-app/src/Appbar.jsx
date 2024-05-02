5e7e687b24f3a8f3b36c50b589bf835381e33921
Tridib11 <tridibghosh12345@gmail.com>
2024-05-02 16:01:44 +0530

    File added

--- week-5/vite-course-selling-app/src/Appbar.jsx
+++ week-5/vite-course-selling-app/src/Appbar.jsx
@@ -3,15 +3,19 @@ import Button from "@mui/material/Button";
 
 function Appbar() {
     return <div style={{
-        display: "flex",
-        justifyContent: "space-between"
+        display: "flex", justifyContent: "space-between"
     }}>
-        <div style={{backgroundColor: "red"}}>
+        <div>
             <Typography variant={"h5"}>Coursera</Typography>
         </div>
-        <div>
-            <Button variant={"contained"}>Sign up</Button>
-            <Button variant={"contained"}>Sign in</Button>
+        <div style={{display:"flex"}}>
+            <div>
+                <Button variant={"contained"}>Sign up</Button>
+            </div>
+            <div>
+                <Button variant={"contained"}>Sign in</Button>
+            </div>
+
         </div>
 
 
