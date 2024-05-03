d0c854904f85e1ee849558aa57075113438ea6b3
Tridib11 <tridibghosh12345@gmail.com>
2024-05-04 03:00:47 +0530

    files added

--- week-5/vite-course-selling-app-frontend/src/AddCourse.jsx
+++ week-5/vite-course-selling-app-frontend/src/AddCourse.jsx
@@ -7,68 +7,71 @@ function AddCourse() {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
     return (
-        <div>
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "570px"
+        }}>
             <div>
                 <Typography
                     variant={"h6"}
-                    style={{display: "flex", justifyContent: "center", margin: "10px"}}
+                    style={{display: "flex", justifyContent: "center", margin: "10px",}}
                 >
                     Add New Course
                 </Typography>
-                <div style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "570px"
-                }}>
-                    <Card variant={"outlined"} style={{width: "400px", padding: "20px"}}>
-                        <TextField
-                            onChange={(e) => {
-                                setTitle(e.target.value)
-                            }}
-                            fullWidth
-                            label="Title"
-                            variant="outlined"
-                        />
-                        <br/> <br/>
-                        <TextField
-                            onChange={(e) => {
-                                setDescription(e.target.value)
-                            }}
-                            fullWidth
-                            label="Description"
-                            variant="outlined"
-                        />
-                        <br/>
-                        <br/>
-                        <div style={{display: "flex", justifyContent: "center"}}>
-                            <Button size={"large"} variant="contained" onClick={() => {
-                                function callback2(data) {
-                                    localStorage.setItem("token", data.token)
-                                    // console.log(data)
+
+                <Card variant={"outlined"} style={{width: "400px", padding: "20px"}}>
+                    <TextField
+                        onChange={(e) => {
+                            setTitle(e.target.value)
+                        }}
+                        fullWidth
+                        label="Title"
+                        variant="outlined"
+                    />
+                    <br/> <br/>
+                    <TextField
+                        onChange={(e) => {
+                            setDescription(e.target.value)
+                        }}
+                        fullWidth
+                        label="Description"
+                        variant="outlined"
+                    />
+                    <br/>
+                    <br/>
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                        <Button size={"large"} variant="contained" onClick={() => {
+                            function callback2(data) {
+                                localStorage.setItem("token", data.token)
+                                // console.log(data)
+                            }
+
+                            function callback1(res) {
+                                res.json().then(callback2)
+                            }
+
+                            fetch("http://localhost:3000/admin/courses", {
+                                method: "POST",
+                                body: JSON.stringify({
+                                    title: title,
+                                    description: description,
+                                    imageLink: "",
+                                    published: true
+
+                                }),
+                                headers: {
+                                    "Content-type": "application/json",
+                                    "Authorization": "Bearer " + localStorage.getItem("token")
                                 }
+                            }).then(callback1)
+                        }}>
+                            Add course
+                        </Button>
+                    </div>
+                </Card>
 
-                                function callback1(res) {
-                                    res.json().then(callback2)
-                                }
-
-                                fetch("http://localhost:3000/admin/signup", {
-                                    method: "POST",
-                                    body: JSON.stringify({
-                                        username: email,
-                                        password: password
-                                    }),
-                                    headers: {
-                                        "Content-type": "application/json"
-                                    }
-                                }).then(callback1)
-                            }}>
-                                Add course
-                            </Button>
-                        </div>
-                    </Card>
-
-                </div>
 
             </div>
         </div>
