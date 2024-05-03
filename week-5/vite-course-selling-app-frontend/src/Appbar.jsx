25e94c94f1953f0d491346fe4d973860680ba931
Tridib11 <tridibghosh12345@gmail.com>
2024-05-04 03:06:25 +0530

    Refactor Appbar component in App.jsx to use react-router-dom for navigation

--- week-5/vite-course-selling-app-frontend/src/Appbar.jsx
+++ week-5/vite-course-selling-app-frontend/src/Appbar.jsx
@@ -1,42 +1,44 @@
-import { Typography } from "@mui/material";
+import {Typography} from "@mui/material";
 import Button from "@mui/material/Button";
+import {useNavigate} from "react-router-dom";
 
 function Appbar() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        padding: "4px",
-      }}
-    >
-      <div>
-        <Typography variant={"h5"}>Coursera</Typography>
-      </div>
-        <div style={{display: "flex"}}>
-            <div style={{marginRight: "10px"}}>
-                <Button
-                    variant={"contained"}
-                    onClick={() => {
-                        window.location = "/signup";
-                    }}
-                >
-                    Sign up
-                </Button>
+    const navigate = useNavigate();
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "4px",
+            }}
+        >
+            <div>
+                <Typography variant={"h5"}>Coursera</Typography>
             </div>
-            <div style={{marginRight: "10px"}}>
-                <Button variant={"contained"} onClick={() => {
-                    window.location = "/signin"
-                }}>Sign in</Button>
-            </div>
-            <div style={{marginRight: "10px"}}>
-                <Button variant={"contained"} onClick={() => {
-                    window.location = "/addcourses"
-                }}>Add Course</Button>
+            <div style={{display: "flex"}}>
+                <div style={{marginRight: "10px"}}>
+                    <Button
+                        variant={"contained"}
+                        onClick={() => {
+                            navigate("/signup")
+                        }}
+                    >
+                        Sign up
+                    </Button>
+                </div>
+                <div style={{marginRight: "10px"}}>
+                    <Button variant={"contained"} onClick={() => {
+                        navigate("/signin")
+                    }}>Sign in</Button>
+                </div>
+                <div style={{marginRight: "10px"}}>
+                    <Button variant={"contained"} onClick={() => {
+                        navigate("/addcourses")
+                    }}>Add Course</Button>
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Appbar;
