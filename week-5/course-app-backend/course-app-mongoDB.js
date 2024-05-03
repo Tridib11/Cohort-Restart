3432ed0adc38017b2e65d28929d917cd1185f946
Tridib11 <tridibghosh12345@gmail.com>
2024-05-03 17:34:16 +0530

    files added

--- week-5/course-app-backend/course-app-mongoDB.js
+++ week-5/course-app-backend/course-app-mongoDB.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+const cors=require("cors")
 const app = express();
 const bodyParser = require("body-parser");
+
+
+app.use(cors())
 app.use(express.json());
 
 app.use(bodyParser.json());
