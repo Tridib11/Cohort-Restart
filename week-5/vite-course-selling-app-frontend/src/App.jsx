43e6f9562e092b6fb6c14dfca91b58ab7da2b264
Tridib11 <tridibghosh12345@gmail.com>
2024-05-04 03:06:21 +0530

    Add Appbar component to main App.jsx file

--- week-5/vite-course-selling-app-frontend/src/App.jsx
+++ week-5/vite-course-selling-app-frontend/src/App.jsx
@@ -7,8 +7,9 @@ import AddCourse from "./AddCourse.jsx";
 function App() {
     return (
         <div style={{width: "100vw", height: "100vh", backgroundColor: "#eeeeee"}}>
-            <Appbar/>
+
             <Router>
+                <Appbar/>
                 <Routes>
                     <Route path="/signin" element={<Signin/>}/>
                     <Route path="/signup" element={<Signup/>}/>
