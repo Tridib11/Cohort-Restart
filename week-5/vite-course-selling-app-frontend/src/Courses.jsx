963f3f16242e958b696ae0af88f4e22d9d376784
Tridib11 <tridibghosh12345@gmail.com>
2024-05-09 22:36:11 +0530

    files added

--- week-5/vite-course-selling-app-frontend/src/Courses.jsx
+++ week-5/vite-course-selling-app-frontend/src/Courses.jsx
@@ -1,4 +1,21 @@
+import {useEffect, useState} from "react";
+
 function Courses(){
+    const [courses,setCourses] = useState([]);
+    useEffect(() => {
+        function callback2(data){
+            console.log(data)
+        }
+        function callback1(res){
+            res.json().then(callback2)
+        }
+        fetch("http://localhost:3000/admin/courses",{
+            method:"GET",
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        }).then(callback1)
+    }, []);
     return (
         <div>
             courses
