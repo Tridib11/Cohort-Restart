2bd067dba6d8f6887891ee8eaf971e069d066d77
Tridib11 <tridibghosh12345@gmail.com>
2024-05-10 01:06:41 +0530

    files added

--- week-5/vite-course-selling-app-frontend/src/App.jsx
+++ week-5/vite-course-selling-app-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Signup from "./Signup.jsx";
 import Appbar from "./Appbar.jsx";
 import AddCourse from "./AddCourse.jsx";
 import Courses from "./Courses.jsx";
+import Course from "./Course.jsx";
 function App() {
     return (
         <div style={{width: "100vw", height: "100vh", backgroundColor: "#eeeeee"}}>
@@ -15,6 +16,7 @@ function App() {
                     <Route path="/signup" element={<Signup/>}/>
                     <Route path="/admin/addcourses" element={<AddCourse/>}/>
                     <Route path="/courses" element={<Courses/>}/>
+                    <Route path="/course/:courseId" element={<Course/>}/>
                 </Routes>
             </Router>
         </div>
